0aa644c0309487c1435dfb0b8a7fe1118059c5f4
Waseem Brare <waseembrare@gmail.com>
2021-02-08 14:46:49 +0000

    Changed the db to not store images but rather the relative paths to images. Updated the speed-factor filed to speed_factor for uniformity. Connected my db to the index.php file and displayed text on page to prove the connection is working.

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
+$db = new PDO('mysql:host=db; dbname=food-collection', 'root', 'password');
+$query = $db->prepare("SELECT * FROM `food`;");
+$query->setFetchMode(PDO::FETCH_ASSOC);
+$query->execute();
+
+$results = $query->fetchAll();
+
+'<ul>';
+foreach ($results as $row) {
+    echo '<br>';
+    echo '<li>' . $row["id"] . ": " . $row["dish_name"]. ", " . $row["image_url"]. ", ". $row["alt_tag"]. ", ". $row["food_creator"]. ", ". $row["ease_factor"]. ", ". $row["speed_factor"]. ", ". $row["taste_factor"]. ", ". $row["health_factor"]. ", ". $row["notes"]. '</li>';
+}
+
 ?>
 
 <!DOCTYPE html>
