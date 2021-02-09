341b654554a6642d8c355e73296c9e4b73e212b8
Waseem Brare <waseembrare@gmail.com>
2021-02-09 16:51:38 +0000

    Added while loop to aut generate divs

--- index.php
+++ index.php
@@ -1,26 +1,42 @@
 <?php
 
-//$db = new PDO('mysql:host=db; dbname=food-collection', 'root', 'password');
-//$query = $db->prepare("SELECT * FROM `food`;");
-//$query->setFetchMode(PDO::FETCH_ASSOC);
-//$query->execute();
-//
-//$recipes = $query->fetchAll();
+$db = new PDO('mysql:host=db; dbname=food-collection', 'root', 'password');
+$query = $db->prepare("SELECT * FROM `food`;");
+$query->setFetchMode(PDO::FETCH_ASSOC);
+$query->execute();
 
+//var_dump($results);
 //'<ul>';
-//foreach ($recipes as $fields) {
+//foreach ($results as $row) {
 //    echo '<br>';
 //    echo '<li>' . $row["id"] . ": " . $row["dish_name"]. ", " . $row["image_url"]. ", ". $row["alt_tag"]. ", ". $row["food_creator"]. ", ". $row["ease_factor"]. ", ". $row["speed_factor"]. ", ". $row["taste_factor"]. ", ". $row["health_factor"]. ", ". $row["notes"]. '</li>';
-//</ul>
+//    '</ul>';
 //}
 
-
-?>
+//?>
+<!---->
+<?// while ($row = $query->fetch()) { ?>
+<!--    <div class="collection_item ">-->
+<!--        <div class="stats">-->
+<!--                    <span class="image_wrapper">-->
+<!--                        <img src="--><?//=$row['image']?><!--">-->
+<!--                    </span>-->
+<!--            <h3>--><?//=$row['name']?><!--</h3>-->
+<!--            <ul>-->
+<!--                <li class="origin">Origin: --><?//=$row['origin']?><!--</li>-->
+<!--                <li class="family">Family: --><?//=$row['family']?><!--</li>-->
+<!--                <li class="preservation_type">--><?//=$row['method']?><!--</li>-->
+<!--                <li class="quality">Rating: --><?//=$row['rating']?><!--/10</li>-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--    </div>-->
+<?// } ?>
 
 <!DOCTYPE html>
 <html lang="en-GB">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>cheffin' it up bruv</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet">
@@ -37,56 +53,27 @@
     <div class="site-intro">
         <h2>add your own plates using the &#x2795 icon or just check out other plates for some tasty inspiration.</h2>
     </div>
-<?//
-//    foreach($recipes as $field) {
-//        print "<div class="collection-item-container">";
-//    }
-//    ?>
-
     <div class="items-outer-container">
-        <div class="collection-item-container">
-            <div class="item-title">
-                <h3><span class="highlight">dish title</h3>
-            </div>
-            <div class="item-image">
-                <img src="https://dummyimage.com/360x360/999999/fff.jpg">
-            </div>
-            <div class="item-stats">
-                <ul>
-                    <li>stat 1</li>
-                    <li>stat 3</li>
-                    <li>stat 4</li>
-                    <li>stat 4</li>
-                </ul>
-            </div>
-            <div class="item-notes">
-                <p>This is a box for short notes section. Character limit 280 (size of tweet)</p>
-            </div>
-            <div class="item-icons">
-                <div class="icon-container">
-                    <img class="icon icon-animation" src="assets/delete-icon-v2.png">
-                    <img class="icon icon-animation" src="assets/edit-icon-v2.png">
-                </div>
-            </div>
-        </div>
 
+    <? while ($row = $query->fetch()) { ?>
         <div class="collection-item-container">
             <div class="item-title">
-                <h3><span class="highlight">dish title</h3>
+                <h3><span class="highlight"><?=$row['dish_name']?></span></h3>
             </div>
             <div class="item-image">
-                <img src="https://dummyimage.com/360x360/999999/fff.jpg">
+                <img src="<?=$row['image_url']?>">
             </div>
             <div class="item-stats">
                 <ul>
-                    <li>stat 1</li>
-                    <li>stat 3</li>
-                    <li>stat 4</li>
-                    <li>stat 4</li>
+                    <li>ease factor: <?=$row['ease_factor']?></li>
+                    <li>speed factor: <?=$row['speed_factor']?></li>
+                    <li>taste factor: <?=$row['taste_factor']?></li>
+                    <li>health factor: <?=$row['health_factor']?></li>
                 </ul>
             </div>
             <div class="item-notes">
-                <p>This is a box for short notes section. Character limit 280 (size of tweet)</p>
+                <p><span class="highlight">notes from <?=$row['food_creator']?>:</span></p>
+                <p><?=$row['notes']?></p>
             </div>
             <div class="item-icons">
                 <div class="icon-container">
@@ -95,34 +82,10 @@
                 </div>
             </div>
         </div>
-
-        <div class="collection-item-container">
-            <div class="item-title">
-                <h3><span class="highlight">dish title</h3>
-            </div>
-            <div class="item-image">
-                <img src="https://dummyimage.com/360x360/999999/fff.jpg">
-            </div>
-            <div class="item-stats">
-                <ul>
-                    <li>stat 1</li>
-                    <li>stat 3</li>
-                    <li>stat 4</li>
-                    <li>stat 4</li>
-                </ul>
-            </div>
-            <div class="item-notes">
-                <p>This is a box for short notes section. Character limit 280 (size of tweet)</p>
-            </div>
-            <div class="item-icons">
-                <div class="icon-container">
-                    <img class="icon icon-animation" src="assets/delete-icon-v2.png">
-                    <img class="icon icon-animation" src="assets/edit-icon-v2.png">
-                </div>
-            </div>
-        </div>
+    <? } ?>
 
     </div>
+
     <div class="add-item-button">
         <img class="action-icon icon-animation" src="assets/plus-icon-v2.png">
     </div>
@@ -132,3 +95,8 @@
 </body>
 </html>
 
+<?php
+
+function deleteItem() {
+
+}
